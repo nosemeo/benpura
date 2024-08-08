930bc3917dfca7e9060bfd6f879a48a49939374d
3030845 <3030845@30308-45>
2024-08-08 11:27:59 +0900

    htmlの位置の変更。controllerのファイルの追加。

--- src/main/java/com/example/demo/controller/CalenderController.java
+++ src/main/java/com/example/demo/controller/CalenderController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("calender")
+@RequestMapping("demo")
 public class CalenderController {
 	
 	@GetMapping
 	public String showList() {
-	return "calender";
+	return "/calender";
 	}
 }
