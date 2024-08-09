a8f01f87bc7f662ea83214f9700bcd024f3b8459
3030736 <3030736@30307-36>
2024-08-09 15:56:14 +0900

    aa

--- src/main/java/com/example/demo/controller/CartController.java
+++ src/main/java/com/example/demo/controller/CartController.java
@@ -1,15 +1,45 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @SessionAttributes("")
 
 public class CartController {
-@GetMapping("/cart")
-public String cartShow() {
-	return "settlement/cart";
-}
+	private HttpSession session;
+
+	@Autowired
+	public CartController(HttpSession session) {
+		// フィールドに代入する
+		this.session = session;
+	}
+
+	@GetMapping("/cart")
+	public String cartShow() {
+		
+		
+		return "settlement/cart";
+	}
+//	@GetMapping("/cart")
+//	public String cartShow(CartDto dto,Model model) {
+//		List<CartDto> dtoList = (List<CartDto>) this.session.getAttribute("dtoList");
+//		if(dtoList.isEmpty()) {
+//			dtoList = new ArrayList<>();
+//		}
+//		dtoList.add(dto);
+//		this.session.setAttribute("dtoList", dtoList);
+//		
+//		model.addAttribute("dtoList",dtoList);
+//		
+//		return "settlement/cart";
+//	}
+	@GetMapping("/settlement")
+	public String showSettlement() {
+		return "settlement/settlement";
+	}
 }
