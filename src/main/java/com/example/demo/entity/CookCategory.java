f608d4ab298c4ca81622c9273d7e85e1e782987b
3030735 <3030735@30307-35>
2024-08-15 11:10:00 +0900

    p

---
+++ src/main/java/com/example/demo/entity/CookCategory.java
@@ -0,0 +1,22 @@
+package com.example.demo.entity;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CookCategory {
+	
+	@Id
+	private Integer id;
+	private String item;
+	private String priceS;
+	private String priceM;
+	private String introductions;
+	private byte[] imagePath;
+	
+}
