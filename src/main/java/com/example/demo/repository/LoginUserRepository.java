376bacf3b23ad98bf1446ecdc92fa6084d2b13ef
nosemeo <162550355+nosemeo@users.noreply.github.com>
2024-09-04 13:17:24 +0900

    Delete src directory

--- src/main/java/com/example/demo/repository/LoginUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.entity.PersonalData;
-
-public interface LoginUserRepository extends CrudRepository<PersonalData, String> {
-
-}
